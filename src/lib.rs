8562ba3d187a8d610b146b6536061ff5574647a8
Lain-5OR4 <112557063+Lain-5OR4@users.noreply.github.com>
2024-12-25 23:26:56 +0900

    [WIP]: chapter2

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 use wasm_bindgen::prelude::*;
 // use wasm_bindgen::JsCast;
 use rand::prelude::*;
+use std::rc::Rc;
+use std::sync::Mutex;
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -22,13 +24,25 @@ pub fn main_js() -> Result<(), JsValue> {
         .unwrap();
 
     wasm_bindgen_futures::spawn_local(async move {
-        let (success_tx, success_rx) = futures::channel::oneshot::channel::<()>();
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
         let image = web_sys::HtmlImageElement::new().unwrap();
 
         let callback = Closure::once(move || {
-            success_tx.send(());
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                success_tx.send(Ok(()));
+            }
         });
+
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                error_tx.send(Err(err));
+            }
+        });
+
         image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
         image.set_src("Idle (1).png");
         success_rx.await;
 
@@ -49,6 +63,14 @@ pub fn main_js() -> Result<(), JsValue> {
     */
 
     Ok(())
+}
+
+async fn fetch_json(json_path: &str) -> Result<JsValue, JsValue> {
+    let window = web_sys::window().unwrap();
+    let resp_value = wasm_bindgen_futures::JsFuture::from(window.fetch_with_str(json_path)).await?;
+
+    let resp: web_sys::Response = resp_value.dyn_into()?;
+    wasm_bindgen_futures::JsFuture::from(resp.json()?).await
 }
 
 fn draw_triangle(
