a18f103304b43e7722d04339c91b89c567fed953
Lain-5OR4 <112557063+Lain-5OR4@users.noreply.github.com>
2025-01-22 20:12:42 +0900

    before delete sierpinski

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,26 @@ use wasm_bindgen::prelude::*;
 use rand::prelude::*;
 use std::rc::Rc;
 use std::sync::Mutex;
+use serde::Deserialize;
+use std::collections::HashMap;
+
+#[derive(Deserialize)]
+struct Rect {
+    x: u16,
+    y: u16,
+    w: u16,
+    h: u16,
+}
+
+#[derive(Deserialize)]
+struct Cell {
+    frame: Rect,
+}
+
+#[derive(Deserialize)]
+struct Sheet {
+    frames: HashMap<String, Cell>,
+}
 
 #[wasm_bindgen(start)]
 pub fn main_js() -> Result<(), JsValue> {
@@ -47,6 +67,44 @@ pub fn main_js() -> Result<(), JsValue> {
         success_rx.await;
 
         context.draw_image_with_html_image_element(&image, 0.0, 0.0);
+        let json = fetch_json("rhb.json").await.expect("Failed to fetch JSON");
+        let sheet: Sheet = json.into_serde().expect("Failed to parse JSON");
+
+        let (success_tx, success_rx) = futures::channel::oneshot::channel::<Result<(), JsValue>>();
+        let success_tx = Rc::new(Mutex::new(Some(success_tx)));
+        let error_tx = Rc::clone(&success_tx);
+        let image = web_sys::HtmlImageElement::new().unwrap();
+
+        let callback = Closure::once(move || {
+            if let Some(success_tx) = success_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                success_tx.send(Ok(()));
+            }
+        });
+
+        let error_callback = Closure::once(move |err| {
+            if let Some(error_tx) = error_tx.lock().ok().and_then(|mut opt| opt.take()) {
+                error_tx.send(Err(err));
+            }
+        });
+
+        image.set_onload(Some(callback.as_ref().unchecked_ref()));
+        image.set_onerror(Some(error_callback.as_ref().unchecked_ref()));
+        image.set_src("rhb.png");
+        success_rx.await;
+
+        let sprite = sheet.frames.get("Run (1).png").expect("Call not found");
+        context.draw_image_with_html_image_element_and_sw_and_sh_and_dx_and_dy_and_dw_and_dh(
+            &image,
+            sprite.frame.x.into(),
+            sprite.frame.y.into(),
+            sprite.frame.w.into(),
+            sprite.frame.h.into(),
+            300.0,
+            300.0,
+            sprite.frame.w.into(),
+            sprite.frame.h.into(),
+        );
+
         sierpinski(
             &context,
             [(300.0, 0.0), (0.0, 600.0), (600.0, 600.0)],
